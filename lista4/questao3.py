642c2c56afdc99f630a3df86e7056a12b51347c0
dbrilhante <davisbrilhante@gmail.com>
2019-05-27 09:01:10 -0300

    fim da questao 3 da lista 4

--- lista4/questao3.py
+++ lista4/questao3.py
@@ -3,8 +3,8 @@ from numpy import dot as mult
 from numpy import array as array
 from matplotlib import pyplot as plt
 
-n = 1000
-sqr = math.sqrt(n)
+n = 1024
+sqr = int(math.sqrt(n))
 mixtureTime = 500
 pi0 = [1] + [0 for i in range(n-1)]
 
@@ -13,22 +13,20 @@ piAnel = array([1/n for i in range(n)])
 pAnel = [[0 for j in range(n)] for i in range(n)]
 
 ################# ARVORE ##########################
-piArv = array([0 for i in range(n)])
+den1 = 2*(n-1)
+piArv = [2/den1]+[0 for i in range(n-1)]
 pArv = [[0 for j in range(n)] for i in range(n)]
 pArv[0] = [0.5, 0.25, 0.25]+[0 for i in range(n-3)]
 piArv[0] = 1/(n-1)
-den1 = 2(n-1)
 
 ############### RETICULADO ########################
-piRet = array([0 for i in range(n)])
+piRet = [0 for i in range(n)]
 pRet = [[0 for j in range(n)] for i in range(n)]
-quinas = [0,99,990,999] 
+quinas = [0,sqr,n-sqr,n-1]
 den2 = 4*n*(n-1)
 
 for i in range(n):
         pAnel[i][i] = 0.5
-        pArv[i][i] = 0.5
-        pRet[i][i] = 0.5
 
         if i==n-1:
             pAnel[i][0] = 0.25
@@ -36,42 +34,100 @@ for i in range(n):
             pAnel[i][i+1] = 0.25
         pAnel[i][i-1] = 0.25
 
-        if quinas.count(i)<>0:
+for i in range(n):
+        pRet[i][i] = 0.5
+        ########### RETICULADO ##########
+        #   > Quina
+        if quinas.count(i)!=0:
             piRet[i] = 2/den2
-            if i==0 or i==99:
+            if i==0 or i==sqr-1:
                 pRet[i][i+sqr] = 0.25
-            if i==0 or i==990:
+            if i==0 or i==n-sqr:
                 pRet[i][i+1] = 0.25
-            if i==990 or i==999:
+            if i==sqr-n or i==n-1:
                 pRet[i][i-sqr] = 0.25
-            if i==99 or i==999:
+            if i==sqr-1 or i==n-1:
                 pRet[i][i-1] = 0.25
-        if (i>0 and i<99): 
+        #   > Bordas
+        elif (i>0 and i<sqr-1): 
             piRet[i]=3/den2
             pRet[i][i-1] = 1/6
             pRet[i][i+1] = 1/6
             pRet[i][i+sqr] = 1/6
-        if (i>990 and i<999):
+        elif (i>n-sqr and i<n-1):
             piRet[i]=3/den2
             pRet[i][i-1] = 1/6
             pRet[i][i+1] = 1/6
             pRet[i][i-sqr] = 1/6
+        elif (i%sqr == sqr-1):
+            piRet[i]=3/den2
+            pRet[i][i-1]=1/6
+            pRet[i][i+sqr] = 1/6
+            pRet[i][i-sqr] = 1/6
+        elif (i%sqr == 0):
+            piRet[i]=3/den2
+            pRet[i][i+1]=1/6
+            pRet[i][i+sqr] = 1/6
+            pRet[i][i-sqr] = 1/6
+        #   > Meio
+        else:
+            piRet[i] = 4/den2
+            pRet[i][i+1]=1/8
+            pRet[i][i-1]=1/8
+            pRet[i][i+sqr] = 1/8
+            pRet[i][i-sqr] = 1/8
             
+#        if i>=n/2:
+#           1 
+base = math.log(n,2)
+for i in range(n):
+    pArv[i][i]=1/2
+    if i == 0:
+        pArv[i][i+1]=1/4
+        pArv[i][i+2]=1/4
+    elif i> math.pow(2,base-2):
+        piArv[i]=1/den1
+        pArv[i][int(i/2)]=1/2
+    else:
+        piArv[i]=3/den1
+        pArv[i][int(i/2)]=1/6
+        pArv[i][(2*i)+1]=1/6
+        pArv[i][(2*i)+2]=1/6
 
-        if i>=n/2:
-            
+
+piArv = array(piArv)
+
+piRet = array(piRet)
 
 pi1 = mult(pi0,pAnel)
-piT = pi1
-resAnel = [sum(abs(piT - piAnel))]
-print(resAnel)
+piTAnel = pi1
+resAnel = [sum(abs(piTAnel - piAnel))]
+
+pi1 = mult(pi0,pRet)
+piTRet = pi1
+resRet = [sum(abs(piTRet - piRet))]
+
+pi1 = mult(pi0,pArv)
+piTArv = pi1
+resArv = [sum(abs(piTArv - piArv))]
 
 for t in range(mixtureTime-1):
-    piT = mult(piT, pAnel)
-    resAnel.append(sum(abs(piT-piAnel)))
+    piTAnel = mult(piTAnel, pAnel)
+    resAnel.append(sum(abs(piTAnel-piAnel)))
 
-print(piT)
+    piTRet = mult(piTRet, pRet)
+    resRet.append(sum(abs(piTRet-piRet)))
+
+    piTArv = mult(piTArv, pArv)
+    resArv.append(sum(abs(piTArv-piArv)))
+
+print(piTAnel)
 print(resAnel)
 
-plt.loglog(range(mixtureTime), resAnel)
+plt.grid(True,which="both",ls="-")
+plt.loglog(range(mixtureTime), resAnel,label='Anel')
+plt.loglog(range(mixtureTime), resRet,label='Reticulado')
+plt.loglog(range(mixtureTime), resArv,label='Arvore')
+plt.legend()
+plt.savefig('lista4-q3.png')
 plt.show()
