b3646aac8ba21c606fab4293ccd2620dbbc92860
Davi <davisbrilhante@gmail.com>
2019-05-27 20:06:32 -0300

    realizando a questao 4, corrigindo erros na matriz de transicao do reticulado

---
+++ lista4/questao4.py
@@ -0,0 +1,181 @@
+import math
+from numpy import dot as mult
+from numpy import array as array
+from matplotlib import pyplot as plt
+
+def totalVarDist(alpha,beta):
+    res=0
+    if len(alpha)==len(beta):
+        for i,j in zip(alpha, beta):
+            res += abs(i-j)
+        return res/2
+    else:
+        print("Arrays length does not match!")
+
+N = [16]#,64,128]#[10,50,100]#,300,700,1000,3000,5000,10000]
+T = [[],[],[]]
+epsilon=0.0003
+#mixtureTime = 500
+
+for n in N:
+    sqr = int(math.sqrt(n))
+    pi0 = [1] + [0 for i in range(n-1)]
+    pi0 = array(pi0)
+    t = [1,1,1]
+    condition= False
+
+    ################## ANEL ###########################
+    piAnel = array([1/n for i in range(n)])
+    pAnel = [[0 for j in range(n)] for i in range(n)]
+
+    ################# ARVORE ##########################
+    den1 = 2*(n-1)
+    piArv = [2/den1]+[0 for i in range(n-1)]
+    pArv = [[0 for j in range(n)] for i in range(n)]
+    pArv[0] = [0.5, 0.25, 0.25]+[0 for i in range(n-3)]
+    piArv[0] = 1/(n-1)
+
+    ############### RETICULADO ########################
+    piRet = [0 for i in range(n)]
+    pRet = [[0 for j in range(n)] for i in range(n)]
+    quinas = [0,sqr-1,n-sqr,n-1]
+    den2 = 4*n*(n-1)
+    print(quinas)
+
+    for i in range(n):
+            pAnel[i][i] = 0.5
+
+            if i==n-1:
+                pAnel[i][0] = 0.25
+            else:
+                pAnel[i][i+1] = 0.25
+            pAnel[i][i-1] = 0.25
+
+    for i in range(n):
+            pRet[i][i] = 0.5
+            ########### RETICULADO ##########
+            #   > Quina
+            if quinas.count(i)!=0:
+                piRet[i] = 2/den2
+                if i==0 or i==sqr-1:
+                    pRet[i][i+sqr] = 0.25
+                if i==0 or i==n-sqr:
+                    pRet[i][i+1] = 0.25
+                if i==n-sqr or i==n-1:
+                    pRet[i][i-sqr] = 0.25
+                if i==sqr-1 or i==n-1:
+                    pRet[i][i-1] = 0.25
+            #   > Bordas
+            elif (i>0 and i<sqr-1): 
+                piRet[i]=3/den2
+                pRet[i][i-1] = 1/6
+                pRet[i][i+1] = 1/6
+                pRet[i][i+sqr] = 1/6
+            elif (i>n-sqr and i<n-1):
+                piRet[i]=3/den2
+                pRet[i][i-1] = 1/6
+                pRet[i][i+1] = 1/6
+                pRet[i][i-sqr] = 1/6
+            elif (i%sqr == sqr-1):
+                piRet[i]=3/den2
+                pRet[i][i-1]=1/6
+                pRet[i][i+sqr] = 1/6
+                pRet[i][i-sqr] = 1/6
+            elif (i%sqr == 0):
+                piRet[i]=3/den2
+                pRet[i][i+1]=1/6
+                pRet[i][i+sqr] = 1/6
+                pRet[i][i-sqr] = 1/6
+            #   > Meio
+            else:
+                piRet[i] = 4/den2
+                pRet[i][i+1]=1/8
+                pRet[i][i-1]=1/8
+                pRet[i][i+sqr] = 1/8
+                pRet[i][i-sqr] = 1/8
+
+    print(pRet)                
+    print(piRet)
+
+    base = math.log(n,2)
+    for i in range(n):
+        pArv[i][i]=1/2
+        if i == 0:
+            pArv[i][i+1]=1/4
+            pArv[i][i+2]=1/4
+        elif i> math.pow(2,base-2):
+            piArv[i]=1/den1
+            pArv[i][int(i/2)]=1/2
+        else:
+            piArv[i]=3/den1
+            pArv[i][int(i/2)]=1/6
+            pArv[i][(2*i)+1]=1/6
+            pArv[i][(2*i)+2]=1/6
+
+
+    piArv = array(piArv)
+
+    piRet = array(piRet)
+
+    pi1 = mult(pi0,pAnel)
+    piTAnel = pi1
+    resAnel = sum(abs(piTAnel - piAnel))
+
+    pi1 = pi0.dot(pRet) #mult(pi0,pRet)
+    piTRet = pi1
+    resRet = totalVarDist(piTRet,piRet)#sum(abs(piTRet - piRet))
+    oldRes = resRet
+    print(resRet)
+    
+    pi1 = mult(pi0,pArv)
+    piTArv = pi1
+    resArv = sum(abs(piTArv - piArv))
+
+    #for t in range(mixtureTime-1):
+    while condition!=True:
+        '''
+        if resAnel > epsilon:
+            condition=False
+            piTAnel = mult(piTAnel, pAnel)
+            resAnel = sum(abs(piTAnel-piAnel))
+            t[0]+=1
+        else: 
+            condition=True
+        '''
+        piTRet = piTRet.dot(pRet)#mult(piTRet, pRet)
+        resRet = totalVarDist(piTRet,piRet) #sum(abs(piTRet-piRet))/2
+        if t[1]%5==0:
+            print(t[1],piTRet,resRet,oldRes)#resRet)
+        if resRet >= epsilon:# and (oldRes >= resRet):
+            #oldRes = resRet 
+            condition=False
+            t[1] += 1
+        else:
+            condition=True
+        #print(t[1],piTRet,resRet)#resRet)
+        '''
+        if resArv > epsilon: 
+            condition=False
+            piTArv = mult(piTArv, pArv)
+            resArv = sum(abs(piTArv-piArv))
+            t[2]+=1
+        else: 
+            condition=True
+        '''
+    #T[0].append(t[0])
+    T[1].append(t[1])
+    #T[2].append(t[2])
+    
+    print(n," Ok!")
+
+
+print(piTAnel)
+print(resAnel)
+
+plt.grid(True,which="both",ls="-")
+plt.loglog(T[0],label='Anel')
+#plt.loglog(T[1],label='Reticulado')
+plt.loglog(T[2],label='Arvore')
+plt.legend()
+plt.savefig('lista4-q3.png')
+plt.show()
