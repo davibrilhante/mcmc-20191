830bd9f68442f803e23acfd446d29936ccffde8c
dbrilhante <davisbrilhante@gmail.com>
2019-06-06 18:30:37 -0300

    ajuste das reflexoes

--- projeto/classes.py
+++ projeto/classes.py
@@ -3,12 +3,22 @@ from math import log
 import random
 
 pi = math.pi
+quarterPi = pi/4
+halfPi = pi/2
+threeHalfPi =3*pi/2
+twicePi = 2*pi 
 class Antenna:
-    def __init__(self,Type='dir', gain=10, beams=8):
+    def __init__(self,Type='FLAT_TOP', beams=8):
         self.type = Type
-        self.gain = gain
+        self.gain = 0
+        self.efficiency = 1
         self.beams = beams
         self.beamwidth = 2*pi/self.beams
+        self.resolution = 1
+        if Type=='FLAT_TOP':
+            self.setFlatTop()
+        elif Type=='CONE_PLUS_CIRCLE':
+            self.setConePlusCircle()
 
     def setFlatTop(self):
         #Considering an antenna pattern with -180 to 180 
@@ -38,20 +48,25 @@ class Node:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        self.txPower = 24 #transmission power in dBm
+        self.sense = -90 #receiver sensitivity in dBm
+        self.antenna = Antenna()
 
 class Room:
-    def __init__(self,width,height):
+    def __init__(self,width,length):
         self.width = width
-        self.height = heigth
+        self.length = length
+        self.height = 0
         self.nObjects = 0
         self.objects = []
 
 class Object:
-    def __init__(self, material, corners):
+    def __init__(self, x, y, material, corners):
         self.material = material
         self.nCorners = corners
         self.corner = []
-        self.loss = 42
+        self.loss = 42 #loss in db
+        self.heigth = 0
 
     def setCorners(self,cornerArray):
         if len(cornerArray)==self.nCorners:
@@ -94,5 +109,76 @@ class Channel:
         elif self.model == 'ONE_RAY_RICE':
             1
 
-def findAnchor(transmitter, receiver, room):
+def getQuadrant(angle):
+    if angle>=0 and angle<= quarterPi:
+        return 1
+    elif angle>quarterPi and angle<=halfPi:
+        return 2
+    elif angle>halfPi and angle<=threeHalfPi:
+        return 3
+    elif angle>threeHalfPi and angle<= twicePi:
+        return 4
+
+def beam2angle(node, beam):
+    #RETURN THE CENTER OF THE BEAM IN THE COMMON ORIENTATION
+    return (beam+0.5)*node.antenna.beamwidth
+
+def getBestBeam():
     1
+def getReflectionEdges(transmitter, room):
+    #Edge1 = (0,0) - (0,l)
+    edge1 = math.atan2(-transmitter.y,-transmitter.x)
+    #Edge2 = (0,l) - (w,l)
+    edge2 = math.atan2(-transmitter.y,(room.length - transmitter.x))
+    #Edge3 = (w,l) - (w,0)
+    edge3 = math.atan2((room.width - transmitter.y),(room.length - transmitter.x))
+    #Edge4 = (0,w) - (0,0)
+    edge4 = math.atan2((room.width - transmitter.y), -transmitter.x)
+    return [edge1,edge2,edge3,edge4]
+
+def getReflectionPoints(transmitter, room, angle, beam, order):
+    #RETURNS A TUPLE WITH THE LENGTH OF REFLECTED PATH, THE POINT OF REFLECTION
+    #AND THE EDGE WHICH REFLECTED. THE POSITION IN THE ARRAY IS ALSO THE ORDER
+    #OF REFLECTION
+    refOrd = order+1
+    rayCenter = beam2angle(transmitter,beam)
+    corners = getReflectionEdges(transmitter, room)
+#    print([math.degrees(i) for i in corners])
+
+    if angle >= corners[2] and angle < halfPi:
+        point = [transmitter.x + (room.width - transmitter.y)/math.tan(angle), room.width]
+        distance = (room.width - transmitter.y)/math.sin(angle)
+        print("1")
+    elif angle >= halfPi and angle <corners[3]:
+        point = [transmitter.x - (room.width - transmitter.y)*math.tan(angle-halfPi), room.width]
+        distance = (room.width - transmitter.y)/math.sin(angle-halfPi)
+        print("2")
+
+    elif angle >= corners[3] and angle < pi:
+        point = [0, transmitter.y + math.tan(pi - angle)*transmitter.x]
+        distance = (transmitter.x)/math.cos(pi - angle)
+        print("3")
+    elif  angle > pi and (angle - twicePi) < corners[0]:
+        point = [0, transmitter.y - math.tan(angle-pi)*transmitter.x]
+        distance = (transmitter.x)/math.cos(angle-pi)
+        print("4")
+
+    elif (angle-twicePi) >= corners[0] and angle < threeHalfPi:
+        point = [transmitter.x - transmitter.y/math.tan(angle - pi),0]
+        distance = transmitter.y/math.sin(angle - pi)
+        print("5")
+    elif angle >= threeHalfPi and (angle - twicePi )< corners[1]:
+        point = [transmitter.x + transmitter.y*math.tan(angle-threeHalfPi),0]
+        distance = transmitter.y/math.cos(angle)
+        print("6")
+
+    elif (angle - twicePi) >= corners[1] and angle < twicePi:
+        point = [room.lenght, transmitter.y - (room.length - transmitter.x)*math.tan(twicePi-angle)]
+        distance = (room.length - transmitter.x)/math.cos(twicePi - angle)
+        print("7")
+    elif angle >= 0 and angle < corners[2]:
+        point = [room.length, transmitter.y + (room.width - transmitter.x)*math.tan(angle)]
+        distance = (room.length - transmitter.x)/math.cos(angle)
+        print("8")
+
+    return [point, distance, refOrd]
--- projeto/main.py
+++ projeto/main.py
@@ -1,4 +1,7 @@
 import classes
+import math
+
+pi = math.pi
 
 a1 = classes.Antenna()
 a1.setFlatTop()
@@ -7,3 +10,10 @@ a2.setConePlusCircle()
 
 print(a1.gain)
 print(a2.gain)
+
+
+n1 = classes.Node(1,2)
+r1 = classes.Room(3,5)
+angles = [pi/4, 3*pi/4, 5*pi/4, 7*pi/4]
+for a in angles:
+    print(classes.getReflectionPoints(n1,r1,a,0,0))
